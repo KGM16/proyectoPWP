353a6f55cc7743764397a08567972f91e01fc055
Kendall <kendallgomezmolina16@gmail.com>
2025-07-04 09:00:20 -0600

    AAS

---
+++ proyectoPWP/src/main/java/com/proyectoPWP/domain/admin/Administrador.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.proyectoPWP.domain.admin;
+
+import com.proyectoPWP.domain.empleado.Empleado;
+
+/**
+ *
+ * @author kgomezm
+ */
+public class Administrador extends Empleado{
+    
+}
---
+++ proyectoPWP/src/main/java/com/proyectoPWP/domain/empleado/Empleado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.proyectoPWP.domain.empleado;
+
+import com.proyectoPWP.domain.usuario.Usuario;
+
+/**
+ *
+ * @author kgomezm
+ */
+public class Empleado extends Usuario {
+    
+}
